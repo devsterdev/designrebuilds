cd3e2ed17755a95e13ae7cde470966e244774de5
devdeepsharma <sharmadevdeep9@gmail.com>
2026-02-07 11:19:56 +0530

    added some hover effects

--- src/components/GlassCard.jsx
+++ src/components/GlassCard.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 
 const GlassCard = () => {
   return (
-    <div className="h-150 w-90 rounded-4xl bg-black flex justify-center relative pt-5">
-      <div className="relative h-140 w-85 bg-[url('https://framer-ui-components.vercel.app/Halftone.png')] rounded-4xl bg-cover bg-center bg-no-repeat inset-0 z-0">
-        <div className="absolute bottom-0 left-0 right-0 h-55 backdrop-blur-[1px] bg-gradient-to-b from-transparent via-black/90  to-black text-white pl-6 ">
-          <h1 className='text-[23px] font-medium text-white w-60 pb-2'>
+    <div className="h-150 w-90 rounded-4xl bg-black flex justify-center relative pt-5 overflow-hidden">
+      <div className="relative h-140 w-full rounded-t-4xl inset-0 z-0 overflow-hidden group">
+        <div className="absolute inset-0 w-85 ml-2 overflow-hidden">
+          <div
+            className="absolute inset-0 bg-[url('https://framer-ui-components.vercel.app/Halftone.png')]
+            bg-cover bg-center bg-no-repeat
+            transition-transform duration-700 ease-out
+            group-hover:scale-105"
+          />
+        </div>
+
+        
+        <div className="absolute bottom-0 left-0 right-0 h-55 backdrop-blur-[2px] bg-gradient-to-b from-transparent  via-black/90 to-black text-white pl-6 pt-2">  
+          <h1 className='text-[23px] font-medium text-white w-60 pb-2 group-hover:-translate-y-1 transition-transform duration-700 ease-out'>
             Crafting a Timeless Brand Identity
           </h1>
-          <p className='text-[17px] text-neutral-300 w-75'>
+          <p className='text-[17px] text-neutral-400 w-75 group-hover:-translate-y-1 group-hover:text-neutral-300 transition-transform duration-700 ease-out'>
             We're a small team in Berlin doing design work for brands that want to stand out.
           </p>
-          <div className='absolute w-70 bottom-0 pb-2 flex justify-between'>
-            <div>
-              
+          <div className='absolute w-70 bottom-0 pb-2 flex justify-between group-hover:-translate-y-1 transition-transform duration-700 ease-out'>
+            <div className=' text-neutral-400 group-hover:text-neutral-300 transition-transform duration-700 ease-out'>
               5 min read
             </div>
             <div className='flex'>
@@ -21,6 +30,7 @@ const GlassCard = () => {
               320
             </div>
           </div>
+        
         </div>
       </div>
     </ div>
